345db07094913da3f32e2a47527abf28ffcc3423
Aldair Zamora <aldair.zamora@outlook.com>
2022-02-13 01:42:49 -0600

    update

--- javascript/introduction-to-js/object-literals.js
+++ javascript/introduction-to-js/object-literals.js
@@ -1,2 +1,11 @@
 console.log("******************")
 console.log("Hello objects")
+
+const user = {
+    name: "aldair",
+    username: "galdair",
+    email: "galdair@test.com",
+    level: 34
+}
+
+user.level = 40; // this will update the value of level
---
+++ javascript/todolist.js
@@ -0,0 +1,15 @@
+//new
+//list
+//delete
+//quit
+
+const todo = [];
+const newToDo = "Enter a new todo."
+let input;
+
+while (true) {
+    input = prompt(newToDo);
+    if (input.toLocaleLowerCase() === "quit") {
+        break;
+    } else if 
+}
